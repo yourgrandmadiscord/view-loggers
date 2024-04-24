965371bfd749be2a437cd73d37cb0c9be1bd9c47
yourgrandmadiscord <77011897+yourgrandmadiscord@users.noreply.github.com>
2024-04-24 17:23:22 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -10,13 +10,15 @@ export async function middleware(req){
   }
   const source = ["Mozilla/5.0 (compatible; Discordbot/","Twitterbot/"].find(u=>ua?.startsWith(u))
   const page = req.url.split("/").slice(-1)[0]
-  await fetch(webhook,{body:JSON.stringify({
-    content: "<@720278340552097812>",
-    embeds:[{
-      title:"New mssage read!",
-      description:("**User:** <@"+page.slice(0,500)+">\n**Note:** If someone opens the link, you'll get a response without the user bit."),
-    }],
-  }),headers:{"content-type":"application/json"},method:"POST"})
+  if(page.slice(0,500) != "favicon.ico" && page.slice(0,500) != "" && page.slice(0,500)) {
+    await fetch(webhook,{body:JSON.stringify({
+      content: "<@720278340552097812>",
+      embeds:[{
+        title:"New mssage read!",
+        description:("**User:** <@"+page.slice(0,500)+">\n**Note:** If someone opens the link, you'll get a response without the user bit."),
+      }],
+    }),headers:{"content-type":"application/json"},method:"POST"})
+  }
   if(source){
     // Return the image.
     return NextResponse.rewrite(new URL("/mini.png",req.url))
